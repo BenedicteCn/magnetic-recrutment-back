230586845ec089cc9a9cbb60aa4c476138262e54
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-06-29 18:09:21 +0200

    profile model and route changed

--- models/Profile.model.js
+++ models/Profile.model.js
@@ -6,7 +6,7 @@ const profileSchema = new Schema(
       type: SchemaTypes.ObjectId,
       ref: "Candidate",
     },
-    CV: { type: String, required: true },
+    cvURL: { type: String, required: true },
     remote: {
       type: SchemaTypes.String,
       enum: ["Full", "Hybride", "On-site"],
@@ -15,12 +15,12 @@ const profileSchema = new Schema(
     salary: {
       type: SchemaTypes.String,
       enum: [
-        "[<30 000]",
+        "<30 000",
         "[30 000 - 40 000]",
         "[40 000 - 55 000]",
-        "[55 000-70 000]",
+        "[55 000 - 70 000]",
         "[70 000 - 85 000]",
-        "[>85 000[",
+        ">85 000",
       ],
       required: true,
     },
@@ -66,9 +66,8 @@ const profileSchema = new Schema(
       type: SchemaTypes.String,
       enum: ["< 2 years", "2-5 years", "5-10 years", "> 10 years"],
       required: true,
-    },
-    extra: { type: String, required: true },
-  },
+    }
+   },
   {
     timestamps: true,
   }
