272983166171ec264951e43244b8b27cfcbf2db8
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-07-01 18:16:50 +0200

    form is now working

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const isHr = require('../middleware/isHr');
-const isCandidate = require('../middleware/isCandidate');
-const isAuthenticated = require('../middleware/isAuthenticated');
-const fileUploader = require('../config/cloudinary.config');
-const User = require('../models/User.model');
-const Profile = require('../models/Profile.model');
+const router = require("express").Router();
+const isHr = require("../middleware/isHr");
+const isCandidate = require("../middleware/isCandidate");
+const isAuthenticated = require("../middleware/isAuthenticated");
+const upload = require("../config/cloudinary.config.js");
+const User = require("../models/User.model");
+const Profile = require("../models/Profile.model");
 
 //Get all candidate profile
-router.get('/', isAuthenticated, isHr, async (req, res, next) => {
+router.get("/", isAuthenticated, isHr, async (req, res, next) => {
   try {
     const allProfile = await Profile.find();
     res.status(200).json(allProfile);
@@ -30,10 +30,10 @@ router.get('/', isAuthenticated, isHr, async (req, res, next) => {
 //   }
 // });
 // Get profile by job field name
-router.get('/field/:name', async (req, res, next) => {
+router.get("/field/:name", async (req, res, next) => {
   try {
     const profileField = req.params.name;
-    const allProfile = await Recipe.find({ field: profileField });
+    const allProfile = await Profile.find({ field: profileField });
     res.status(200).json(allProfile);
   } catch (err) {
     next(err);
@@ -43,37 +43,79 @@ router.get('/field/:name', async (req, res, next) => {
 //FORM
 
 //to edit with the form
-router.get('/candidate/create', isAuthenticated, isCandidate, (req, res) =>
-  res.render('movie-views/movie-create')
-);
+// router.get("/create", isAuthenticated, isCandidate, (req, res) =>
+//   res.render("/")
+// );
 
-router.post('/create', isAuthenticated, isCandidate, fileUploader.single('document-cv'), (req, res) => {
-  const {
-    remote,
-    salary,
-    contract,
-    position,
-    technologies,
-    experience,
-  } = req.body;
+// router.post(
+//   "/",
+//   upload.single("url"),
+//   isAuthenticated,
+//   isCandidate,
+//   async (req, res, next) => {
+//     try {
+//       if (req.file) {
+//         req.body.image = req.file.path;
+//       }
+//       let postInput = req.body;
+//       postInput.creator = req.user;
+//       const createdPost = await Profile.create(postInput);
+//       res.status(201).json(createdPost);
+//     } catch (error) {
+//       res.status(500).json(error);
+//       next(error);
+//     }
+//   }
+// );
 
-  Profile.create({
-    remote,
-    salary,
-    contract,
-    position,
-    technologies,
-    experience,
-    extra,
-    cvURL: req.file.path,
-  })
-    .then((newlyCreatedDocumentFromDB) => {
-      console.log(newlyCreatedDocumentFromDB);
-      res.redirect('/profile')
-    })
-    .catch((error) =>
-      console.log(`Error while creating a new document: ${error}`)
+router.patch("/create", isAuthenticated, async (req, res, next) => {
+  // const { remote, salary, contract, position, experience } = req.body;
+  console.log(req.user);
+  try {
+    const updatedUser = await Profile.findOneAndUpdate(
+      { candidate: req.user._id },
+      req.body,
+      { new: true, upsert: true }
     );
+    console.log(updatedUser);
+    res.status(200).json(updatedUser);
+  } catch (e) {
+    next(e);
+  }
+  // .then((response) => {
+  //   console.log(response);
+  //   res.json(response);
+  // })
+  // .catch((err) => res.json(err));
 });
+
+// router.post(
+//   "/create",
+//   isAuthenticated,
+//   isCandidate,
+//   fileUploader.single("document-cv"),
+//   (req, res) => {
+//     const { remote, salary, contract, position, technologies, experience } =
+//       req.body;
+
+//     Profile.create({
+//       remote,
+//       salary,
+//       contract,
+//       position,
+//       technologies,
+//       experience,
+//       extra,
+//       cvURL: req.file.path,
+//     })
+//       .then((newlyCreatedDocumentFromDB) => {
+//         console.log(newlyCreatedDocumentFromDB);
+//         res.redirect("/profile");
+//       })
+//       .catch((error) =>
+//         console.log(`Error while creating a new document: ${error}`)
+//       );
+//   }
+// );
 
 module.exports = router;
