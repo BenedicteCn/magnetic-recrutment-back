c01affbd1471a89349db9d44876a959cd7661f0d
Tzu Yu Chiu <tzuyujulia@gmail.com>
2022-07-01 12:26:46 +0200

    fixed github helper & passportJS & candidate route

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,38 @@
 const GitHubStrategy = require("passport-github2").Strategy;
 const { Candidate } = require("../models/User.model");
 const passport = require("passport");
-const { getGithubEmails } = require("../helpers/github");
+const { getGithubEmails, getGithubProfileInfo } = require("../helpers/github");
 const GithubProfile = require("../models/GithubProfile.model.js");
 
 const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
+
+const findCandidateByGithubProfileId = async (id) => {
+  const candidate = await Candidate.findOne({ githubId: id });
+  return candidate;
+};
+
+const createGithubProfile = async (candidate, accessToken, username) => {
+  // Get user repo information
+  const info = await getGithubProfileInfo(accessToken);
+  console.log({ info });
+
+  // Store github info for this new user
+  await GithubProfile.create({ ...info, username, candidate });
+};
+
+const createCandidateFromGithub = async (accessToken, { id, username }) => {
+  const emails = await getGithubEmails(accessToken);
+  const candidate = await Candidate.create({
+    email: emails[0].email,
+    githubId: id,
+  });
+
+  // We don't await this because we don't need this info to continue,
+  // we just store it for later.
+  createGithubProfile(candidate, accessToken, username);
+
+  return candidate;
+};
 
 passport.use(
   new GitHubStrategy(
@@ -15,31 +43,17 @@ passport.use(
       scope: "user:email",
     },
     async (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken);
       try {
-        const user = await Candidate.findOne({ githubId: profile.id });
         console.log("LOGIN PROFILE", profile);
-        console.log(user);
+        let candidate = await findCandidateByGithubProfileId(profile.id);
 
-        if (user) {
-          done(null, user);
-          return;
+        if (!candidate) {
+          candidate = await createCandidateFromGithub(accessToken, profile);
         }
-        //Github sign up
-        const emails = await getGithubEmails(accessToken);
-        console.log(emails);
 
-        const candidate = await Candidate.create({
-          email: emails[0].email,
-          githubId: profile.id,
-        });
-
-        // TODO: Get user repo information
-        // Store github info for this new user
-        const info = getGithubInfoForUsername(profile.username);
-        GithubProfile.create({ ...info, candidate });
-        done(null, user);
+        done(null, candidate);
       } catch (error) {
+        console.log(error);
         done(error);
       }
     }
--- seed/users.seed.js
+++ seed/users.seed.js
@@ -8,29 +8,19 @@ const bcrypt = require("bcryptjs");
 
 const users = [
   {
-    username: "Benedicte Coulon",
-    github_name: "benedicteCn",
+    username: "benedicteCn",
     email: "benedicte.coulon@gmail.com",
     password: bcrypt.hashSync("bene123", 10),
     role: "Candidate",
   },
   {
-    username: "Tzu Yu Chiu",
-    github_name: "tzuyuchiu",
+    username: "tzuyuchiu",
     email: "tzuyu.chiu@gmail.com",
     password: bcrypt.hashSync("tzuyu123", 10),
     role: "Candidate",
   },
   {
-    username: "Stephane Deias",
-    github_name: "StephaneDs",
-    email: "Stephane.Deias@gmail.com",
-    password: bcrypt.hashSync("stephane123", 10),
-    role: "Candidate",
-  },
-  {
-    username: "Stephane Deias",
-    github_name: "StephaneDs",
+    username: "StephaneDs",
     email: "Stephane.Deias@gmail.com",
     password: bcrypt.hashSync("stephane123", 10),
     role: "Candidate",
