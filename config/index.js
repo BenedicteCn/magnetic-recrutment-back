56c402acbd82281eb3774aa4a4a125f0fcc78289
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-07-06 18:08:48 +0200

    deployment

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
+      origin: process.env.ORIGIN || "https://magnetic-recruitment.netlify.app/",
     })
   );
 
